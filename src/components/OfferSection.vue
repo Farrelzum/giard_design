e2aeafbc51baf570113f7bde4905f8643aa554c0
Grzegorz <grze.pra@gmail.com>
2026-07-27 16:34:56 +0200

    refactor: extract offers data to a separate file

--- src/components/OfferSection.vue
+++ src/components/OfferSection.vue
@@ -1,28 +1,5 @@
 <script setup>
-import iconProject from '../assets/icons/pencil.svg'
-import iconEye from '../assets/icons/eye.svg'
-import iconStars from '../assets/icons/stars.svg'
-
-const offers = [
-  {
-    title: 'Projekty',
-    description: 'Zaprojektujemy Twój ogród w nowoczesnym stylu i z najlepszym wykorzystaniem istniejącej przestrzeni.',
-    linkText: 'Dowiedz się więcej',
-    icon: iconProject
-  },
-  {
-    title: 'Wizualizacje',
-    description: 'Przedstawimy Ci projekty koncepcyjne w postaci wirtualnego spaceru animowanego w technologii 3D.',
-    linkText: 'Dowiedz się więcej',
-    icon: iconEye
-  },
-  {
-    title: 'Realizacje',
-    description: 'Zrealizujemy Twoje marzenie przy użyciu najnowszych rozwiązań i zaawansowanych technologii.',
-    linkText: 'Zobacz nasze realizacje',
-    icon: iconStars
-  }
-]
+    import { offers } from '../data/offers.js'
 </script>
 
 <template>
---
+++ src/data/offers.js
@@ -0,0 +1,24 @@
+import iconProject from '../assets/icons/pencil.svg'
+import iconEye from '../assets/icons/eye.svg'
+import iconStars from '../assets/icons/stars.svg'
+
+export const offers = [
+  {
+    title: 'Projekty',
+    description: 'Zaprojektujemy Twój ogród w nowoczesnym stylu i z najlepszym wykorzystaniem istniejącej przestrzeni.',
+    linkText: 'Dowiedz się więcej',
+    icon: iconProject
+  },
+  {
+    title: 'Wizualizacje',
+    description: 'Przedstawimy Ci projekty koncepcyjne w postaci wirtualnego spaceru animowanego w technologii 3D.',
+    linkText: 'Dowiedz się więcej',
+    icon: iconEye
+  },
+  {
+    title: 'Realizacje',
+    description: 'Zrealizujemy Twoje marzenie przy użyciu najnowszych rozwiązań i zaawansowanych technologii.',
+    linkText: 'Zobacz nasze realizacje',
+    icon: iconStars
+  }
+]
