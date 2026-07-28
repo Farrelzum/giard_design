9aa7505fb666cd7695ac49e94975112761207816
Grzegorz <grze.pra@gmail.com>
2026-07-28 14:39:45 +0200

    refactor: extract slides data to separate file

--- src/components/HeroSlider.vue
+++ src/components/HeroSlider.vue
@@ -1,20 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import arrangement_slide from '../assets/images/arrangement_slide.png'
-import irrigation_slide from '../assets/images/irrigation_slide.png'
-
-const slides = [
-  {
-    title: 'Nowoczesna\naranżacja\nTwojego ogrodu',
-    description: 'Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.',
-    image: arrangement_slide
-  },
-  {
-    title: 'Profesjonalne\nsystemy\nnawadniania',
-    description: 'Oszczędzaj wodę i czas. Instalujemy inteligentne systemy nawadniające, które same dbają o idealną wilgotność Twojego trawnika.',
-    image: irrigation_slide
-  }
-]
+import { slides } from '../data/slides.js'
 
 const currentSlide = ref(0)
 
---
+++ src/data/slides.js
@@ -0,0 +1,16 @@
+// src/data/slides.js
+import arrangement_slide from '../assets/images/arrangement_slide.png'
+import irrigation_slide from '../assets/images/irrigation_slide.png'
+
+export const slides = [
+  {
+    title: 'Nowoczesna\naranżacja\nTwojego ogrodu',
+    description: 'Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.',
+    image: arrangement_slide
+  },
+  {
+    title: 'Profesjonalne\nsystemy\nnawadniania',
+    description: 'Oszczędzaj wodę i czas. Instalujemy inteligentne systemy nawadniające, które same dbają o idealną wilgotność Twojego trawnika.',
+    image: irrigation_slide
+  }
+]
